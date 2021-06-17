14a40b68263422d70dd7bcd4c87b258fefeb091e
Caio <caiofmart@gmail.com>
2021-06-17 18:10:59 -0300

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Caio Feitoza Martins on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
